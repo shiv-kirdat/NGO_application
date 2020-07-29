11f094e88ddb251a2891f73a242a93ca25ada777
vinaya-kulkarni <vinayakulkarni012@gmail.com>
2020-07-29 08:10:49 +0530

    button added

--- app/src/main/java/com/example/ngo_application/MainActivity.java
+++ app/src/main/java/com/example/ngo_application/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.ngo_application;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Bundle savedInstanceState;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.savedInstanceState = savedInstanceState;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
