6a770eb3bd1341467878a95d0472aad7ef440284
sayali1505 <patilsayali1505@gmail.com>
2020-08-05 13:00:21 +0530

    commit for create event page

---
+++ admin_side/src/main/java/com/example/admin_side/Create_event.java
@@ -0,0 +1,14 @@
+package com.example.admin_side;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Create_event extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+}
--- admin_side/src/main/java/com/example/admin_side/adminActivity.java
+++ admin_side/src/main/java/com/example/admin_side/adminActivity.java
@@ -2,7 +2,11 @@ package com.example.admin_side;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class adminActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class adminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        Button create_event=findViewById(R.id.create_event);
+        create_event.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent("Create_event");
+                startActivity(i);
+            }
+        });
     }
+
+
 }
